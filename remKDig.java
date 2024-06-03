8deb906ccf9f62f8fcb4d7cb651963451dd03d7b
Jayavani9 <jayavaniakkiraju09@gmail.com>
2024-06-02 18:21:01 -0700

    Updated

---
+++ remKDig.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+        // Tc: O(n) Sc: O(n)
+        int len = num.length();
+        if (k == len) {
+            return "0";
+        }
+
+        Stack<Character> stack = new Stack<>();
+        int i = 0;
+        while (i < len) {
+
+            while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(i)) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+            i++;
+        }
+
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+
+        return sb.toString();
+
+    }
+}
